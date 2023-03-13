43bd75fe228be7e7884cfb6b3da7ced13662598c
facumarquez <facumarquez@gmail.com>
2023-03-13 22:07:03 +0000

    Add frontend pages

--- frontend-react-js/src/components/ProfileInfo.js
+++ frontend-react-js/src/components/ProfileInfo.js
@@ -2,6 +2,7 @@ import './ProfileInfo.css';
 import {ReactComponent as ElipsesIcon} from './svg/elipses.svg';
 import React from "react";
 
+// [TODO] Authenication
 import { Auth } from 'aws-amplify';
 
 export default function ProfileInfo(props) {
@@ -15,6 +16,7 @@ export default function ProfileInfo(props) {
     try {
         await Auth.signOut({ global: true });
         window.location.href = "/"
+        localStorage.removeItem("access_token")
     } catch (error) {
         console.log('error signing out: ', error);
     }
