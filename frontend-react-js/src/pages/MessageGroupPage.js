1495f380188dac5fcd05871853f1beb7f21c81c6
facumarquez <facumarquez@gmail.com>
2023-03-29 23:44:48 +0000

    Update frontend

--- frontend-react-js/src/pages/MessageGroupPage.js
+++ frontend-react-js/src/pages/MessageGroupPage.js
@@ -1,27 +1,23 @@
-import './MessageGroupPage.css';
+import './MessageGroupsPage.css';
 import React from "react";
-import { useParams } from 'react-router-dom';
 
 import DesktopNavigation  from '../components/DesktopNavigation';
 import MessageGroupFeed from '../components/MessageGroupFeed';
-import MessagesFeed from '../components/MessageFeed';
-import MessagesForm from '../components/MessageForm';
+import checkAuth from '../lib/CheckAuth';
 
-// [TODO] Authenication
-import Cookies from 'js-cookie'
-
-export default function MessageGroupPage() {
+export default function MessageGroupsPage() {
   const [messageGroups, setMessageGroups] = React.useState([]);
-  const [messages, setMessages] = React.useState([]);
   const [popped, setPopped] = React.useState([]);
   const [user, setUser] = React.useState(null);
   const dataFetchedRef = React.useRef(false);
-  const params = useParams();
 
-  const loadMessageGroupsData = async () => {
+  const loadData = async () => {
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/message_groups`
       const res = await fetch(backend_url, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`
+        },
         method: "GET"
       });
       let resJson = await res.json();
@@ -35,43 +31,13 @@ export default function MessageGroupPage() {
     }
   };  
 
-  const loadMessageGroupData = async () => {
-    try {
-      const handle = `@${params.handle}`;
-      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/messages/${handle}`
-      const res = await fetch(backend_url, {
-        method: "GET"
-      });
-      let resJson = await res.json();
-      if (res.status === 200) {
-        setMessages(resJson)
-      } else {
-        console.log(res)
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };  
-
-  const checkAuth = async () => {
-    console.log('checkAuth')
-    // [TODO] Authenication
-    if (Cookies.get('user.logged_in')) {
-      setUser({
-        display_name: Cookies.get('user.name'),
-        handle: Cookies.get('user.username')
-      })
-    }
-  };
-
   React.useEffect(()=>{
     //prevents double call
     if (dataFetchedRef.current) return;
     dataFetchedRef.current = true;
 
-    loadMessageGroupsData();
-    loadMessageGroupData();
-    checkAuth();
+    loadData();
+    checkAuth(setUser);
   }, [])
   return (
     <article>
@@ -79,9 +45,7 @@ export default function MessageGroupPage() {
       <section className='message_groups'>
         <MessageGroupFeed message_groups={messageGroups} />
       </section>
-      <div className='content messages'>
-        <MessagesFeed messages={messages} />
-        <MessagesForm setMessages={setMessages} />
+      <div className='content'>
       </div>
     </article>
   );
