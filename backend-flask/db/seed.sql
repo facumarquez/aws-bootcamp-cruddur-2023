541e1467cf1dadb40bc41b3dc2322de1d15c1db5
facumarquez <facumarquez@gmail.com>
2023-03-30 02:43:03 +0000

    Modify pages

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,12 +2,13 @@
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
   ('Andrew Brown', 'andrewbrown', 'andrew@test.com' ,'MOCK'),
-  ('Andrew Bayko', 'bayko', 'bayko@testemail.com' ,'MOCK');
+  ('Andrew Bayko', 'bayko', 'bayko@testemail.com' ,'MOCK'),
+  ('Facundo Marquez', 'facumarquez', 'facumarquez@gmail.com' ,'22676402-82eb-46f2-b9d4-2488f6caf942');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'facumarquez' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
