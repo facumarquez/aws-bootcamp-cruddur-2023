63ff4435550f5add70524da98581cc13e55817cd
facumarquez <facumarquez@gmail.com>
2023-04-03 14:08:04 +0000

    Fix seed script

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -3,7 +3,7 @@ INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
   ('Andrew Brown', 'andrewbrown', 'andrew@test.com' ,'MOCK'),
   ('Andrew Bayko', 'bayko', 'bayko@testemail.com' ,'MOCK'),
-  ('Facundo Marquez', 'facumarquez', 'facumarquez@gmail.com' ,'22676402-82eb-46f2-b9d4-2488f6caf942');
+  ('Facundo Marquez', 'facumarquez', 'facumarquez@gmail.com' ,'22676402-82eb-46f2-b9d4-2488f6caf942'),
   ('Londo Mollari','londo' ,'lmollari@centari.com' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
