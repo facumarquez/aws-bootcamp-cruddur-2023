bdbe365430e29505f5d3d9c328a2a399b47279fa
facumarquez <facumarquez@gmail.com>
2023-05-17 21:06:42 +0000

    Working locally

--- frontend-react-js/src/components/ProfileHeading.js
+++ frontend-react-js/src/components/ProfileHeading.js
@@ -1,8 +1,6 @@
 import './ProfileHeading.css';
 import EditProfileButton from '../components/EditProfileButton';
 
-import ProfileAvatar from 'components/ProfileAvatar'
-
 export default function ProfileHeading(props) {
   const backgroundImage = 'url("https://assets.facumarquez.com/banners/banner.jpg")';
   const styles = {
@@ -14,17 +12,18 @@ export default function ProfileHeading(props) {
   <div className='activity_feed_heading profile_heading'>
     <div className='title'>{props.profile.display_name}</div>
     <div className="cruds_count">{props.profile.cruds_count} Cruds</div>
-    <div className="banner" style={styles} >
-      <ProfileAvatar id={props.profile.cognito_user_uuid} />
+    <div class="banner" style={styles} >
+      <div className="avatar">
+        <img src="https://assets.facumarquez.com/avatars/data.jpg"></img>
+      </div>
     </div>
-    <div className="info">
-      <div className='id'>
+    <div class="info">
+      <div class='id'>
         <div className="display_name">{props.profile.display_name}</div>
         <div className="handle">@{props.profile.handle}</div>
       </div>
       <EditProfileButton setPopped={props.setPopped} />
     </div>
-    <div className="bio">{props.profile.bio}</div>
 
   </div>
   );
